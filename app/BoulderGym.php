36533ce452e7845fa82f4af9d1f1b71b12c05b1c
Bas0870 <sentinal01@gmail.com>
2020-06-12 21:20:23 +0200

    Add boulder gym migration with model

---
+++ app/BoulderGym.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\BoulderGym
+ *
+ * @property int $id
+ * @property string $name
+ * @property float $lat
+ * @property float $lng
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGym newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGym newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGym query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGym whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGym whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGym whereLat($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGym whereLng($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGym whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGym whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class BoulderGym extends Model
+{
+    //
+}
